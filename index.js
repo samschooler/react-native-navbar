730c653e4629a56abfb35521fc6ced023c71685d
Sam Schooler <me@sam.ink>
2017-04-27 22:14:32 -0600

    exports

---
+++ index.js
@@ -0,0 +1,13 @@
+import NavBar from './NavBar';
+import NavBarFlexItem from './NavBarFlexItem';
+import NavBarItem from './NavBarItem';
+import NavBarLeft from './NavBarLeft';
+import NavBarRight from './NavBarRight';
+import NavBarTitle from './NavBarTitle';
+import NavBarTouchableItem from './NavBarTouchableItem';
+import NavSearchBar from './NavSearchBar';
+
+export default NavBar;
+export {
+  NavBar, NavBarFlexItem, NavBarItem, NavBarLeft, NavBarRight, NavBarTitle, NavBarTouchableItem, NavSearchBar
+};
